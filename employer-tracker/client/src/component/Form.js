b5be26ae9f017a0d60b0e859ebf758398eae40e4
skosana721 <wonganiskosana@gmail.com>
2021-04-13 11:03:10 +0200

    created method for handleChange and pass it to all input onChange method

--- employer-tracker/client/src/component/Form.js
+++ employer-tracker/client/src/component/Form.js
@@ -6,15 +6,29 @@ function Form() {
     surname: "",
     progress: "",
   });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInfo({ ...formInfo, [name]: value });
+  };
   return (
     <div>
       <form>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" onChange={handleChange} />
         <label htmlFor="surname">Surname</label>
-        <input type="text" name="surname" id="surname" />
+        <input
+          type="text"
+          name="surname"
+          id="surname"
+          onChange={handleChange}
+        />
         <label htmlFor="">Progress</label>
-        <input type="text" name="progress" id="progress" />
+        <input
+          type="text"
+          name="progress"
+          id="progress"
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
