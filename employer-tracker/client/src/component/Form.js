390aa15f797292f65c86b0772d725189d0e71460
skosana721 <wonganiskosana@gmail.com>
2021-04-13 11:21:31 +0200

    deconstructed the formInfo and in the handleSubmit created an if statement to cheks if the state value a true before submitting and add value from the state to all the inputs

--- employer-tracker/client/src/component/Form.js
+++ employer-tracker/client/src/component/Form.js
@@ -6,24 +6,40 @@ function Form() {
     surname: "",
     progress: "",
   });
+  const { name, surname, progress } = formInfo;
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormInfo({ ...formInfo, [name]: value });
   };
   const handleSubmit = (e) => {
+    const { name, surname, progress } = formInfo;
     e.preventDefault();
+    if ((name, surname, progress)) {
+      setFormInfo({
+        name: "",
+        surname: "",
+        progress: "",
+      });
+    }
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={handleChange}
+          value={name}
+        />
         <label htmlFor="surname">Surname</label>
         <input
           type="text"
           name="surname"
           id="surname"
           onChange={handleChange}
+          value={surname}
         />
         <label htmlFor="">Progress</label>
         <input
@@ -31,6 +47,7 @@ function Form() {
           name="progress"
           id="progress"
           onChange={handleChange}
+          value={progress}
         />
         <button type="submit">Submit</button>
       </form>
