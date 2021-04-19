b44f0fa8a33b9242a2b78a10af7a468caa1bbc97
skosana721 <wonganiskosana@gmail.com>
2021-04-19 14:20:03 +0200

    added a get request which send the employees file

--- employer-tracker/server/routes/api/employee.js
+++ employer-tracker/server/routes/api/employee.js
@@ -3,10 +3,11 @@ const employees = require("../../employees");
 const employeeRoutes = (app) => {
   app.get("/api/employee", async (req, res) => {
     try {
-      await res.status(200).json(employees);
+      await res.status(200).send(employees);
     } catch (error) {
-      res.status(404).json(error);
+      res.status(404).send(error);
     }
   });
 };
+
 module.exports = { employeeRoutes };
