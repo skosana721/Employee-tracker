6631250cda17632d94551cc06e53410630c5f593
skosana721 <wonganiskosana@gmail.com>
2021-04-20 10:17:17 +0200

    remove the console.log

--- employer-tracker/client/src/redux/actions/form.js
+++ employer-tracker/client/src/redux/actions/form.js
@@ -4,7 +4,6 @@ import { GET_EMPLOYEE } from "../actionTypes/form";
 export const getEmployee = () => {
   return (dispatch) => {
     axios.get("http://localhost:5000/api/employee").then((res) => {
-      console.log("res", res.data);
       dispatch({
         type: GET_EMPLOYEE,
         payload: res.data,
