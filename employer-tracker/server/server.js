2c19ca7df71772444e5646e071a12b807001fd2b
skosana721 <wonganiskosana@gmail.com>
2021-04-20 11:11:48 +0200

    added express bodyparser middleware

--- employer-tracker/server/server.js
+++ employer-tracker/server/server.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 const { employeeRoutes } = require("./routes/api/employee");
 app.use(cors());
 
+// bodyparser middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 employeeRoutes(app);
 
 app.listen(5000, () => console.log(`Server is running on port 5000`));
