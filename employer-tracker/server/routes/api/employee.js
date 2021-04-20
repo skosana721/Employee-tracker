9aa70feaf9001177ba9d33f1139c3b8b5e07e165
skosana721 <wonganiskosana@gmail.com>
2021-04-20 11:13:22 +0200

    created a post request that creates newEmployed with name, surname, progess and it push it to the employees array

--- employer-tracker/server/routes/api/employee.js
+++ employer-tracker/server/routes/api/employee.js
@@ -1,4 +1,5 @@
 const employees = require("../../employees");
+const uuid = require("uuid");
 
 const employeeRoutes = (app) => {
   app.get("/api/employee", async (req, res) => {
@@ -6,6 +7,20 @@ const employeeRoutes = (app) => {
       await res.status(200).send(employees);
     } catch (error) {
       res.status(404).send(error);
+    }
+  });
+  app.post("/api/employee", async (req, res) => {
+    try {
+      const newEmployee = await {
+        id: uuid.v4(),
+        name: req.body.name,
+        surname: req.body.surname,
+        progress: req.body.progress,
+      };
+      employees.push(newEmployee);
+      res.status(201).json(employees);
+    } catch (error) {
+      res.status(400).send(error);
     }
   });
 };
