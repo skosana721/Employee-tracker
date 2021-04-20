dfa5a3e3d5a9218049a37db5dc27cc65a3e54694
skosana721 <wonganiskosana@gmail.com>
2021-04-20 09:46:49 +0200

    created a case for GET_EMPLOYEE which spreads the state and updated the form with the action.payload

--- employer-tracker/client/src/redux/reducer/formReducer.js
+++ employer-tracker/client/src/redux/reducer/formReducer.js
@@ -1,8 +1,15 @@
+import { GET_EMPLOYEE } from "../actionTypes/form";
+
 const initialState = {
-  form: "",
+  form: [],
 };
 export const formReducer = (state = initialState, action) => {
   switch (action) {
+    case GET_EMPLOYEE:
+      return {
+        ...state,
+        form: action.payload,
+      };
     default:
       return state;
   }
