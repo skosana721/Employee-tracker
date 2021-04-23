c4c65040398ae6439661b704a9cd7f0f582462a4
skosana721 <wonganiskosana@gmail.com>
2021-04-23 16:00:39 +0200

    the updateValue function which responsible for edit and passed it as props to the editprogress component

--- employer-tracker/client/src/component/EmployeeProgress.js
+++ employer-tracker/client/src/component/EmployeeProgress.js
@@ -1,61 +1,86 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { deleteEmployee } from "../redux/actions/form";
+import { completed, deleteEmployee, editEmployee } from "../redux/actions/form";
 import { Table } from "reactstrap";
+import EditProgress from "./EditProgress";
 
 function EmployeeProgress() {
   const employees = useSelector((state) => state.form.form);
+
   const dispatch = useDispatch();
-  const [complete, setComplete] = useState(false);
-  const completed = () => {
-    setComplete(!completed);
-    if (complete) {
-      return {
-        textDecoration: "line-through double red",
-      };
-    }
+  const [edit, setEdit] = useState("");
+  const [isEditable, setIsEditable] = useState(false);
+
+  const handelComplete = (id) => {
+    employees.map((employee) => {
+      if (employee.id === id) {
+        let newData = { ...employee, isCompleted: !employee.isCompleted };
+        return dispatch(completed(newData));
+      }
+
+      return employee;
+    });
   };
-  const foundEmployee = (id) => {
-    let found = employees.find((employee) => employee.id === id);
-    if (found) {
-      completed();
+
+  const updateValue = (id, edit) => {
+    let currentProgress;
+    let selectedEmployee = employees.find((employee) => employee.id === id);
+    console.log("selected", selectedEmployee);
+    if (selectedEmployee) {
+      currentProgress = selectedEmployee.progress;
     }
+    currentProgress = edit;
+    return { currentProgress, id };
   };
+  console.log(employees);
+
   return (
-    <Table dark>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Surname</th>
-          <th>Progress</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {employees.length !== 0 &&
-          employees.map((employee) => {
-            const { id, name, surname, progress } = employee;
-
-            return (
-              <tr key={id} style={completed()}>
-                <td>{name}</td>
-                <td>{surname}</td>
-                <td>{progress}</td>
-                {!complete && (
-                  <div>
-                    <button onClick={() => dispatch(deleteEmployee(id))}>
-                      Delete
-                    </button>
-
-                    <button onClick={() => foundEmployee(id)}>Complete</button>
-                  </div>
-                )}
-              </tr>
-            );
-          })}
-      </tbody>
-    </Table>
+    <div className="display-table">
+      <Table dark>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Progress</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.length > 0 &&
+            employees.map((employee) => {
+              const { id, name, surname, progress, isCompleted } = employee;
+              console.log("employee", id);
+              return (
+                <tr key={id} className={`${isCompleted ? "complete" : ""} `}>
+                  <td>{name}</td>
+                  <td>{surname}</td>
+                  <td>
+                    {!isEditable ? (
+                      progress
+                    ) : (
+                      <EditProgress {...employee} updateValue={updateValue} />
+                    )}
+                  </td>
+                  <button onClick={() => dispatch(deleteEmployee(id))}>
+                    Delete
+                  </button>
+                  {
+                    <div>
+                      <button onClick={() => setIsEditable(!isEditable)}>
+                        Edit
+                      </button>
+                      <button onClick={() => handelComplete(id)}>
+                        Complete
+                      </button>
+                    </div>
+                  }
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
