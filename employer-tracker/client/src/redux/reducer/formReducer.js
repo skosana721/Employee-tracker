8dd4db1a503c2ed7f3f38b11f67349c4e081d142
skosana721 <wonganiskosana@gmail.com>
2021-04-28 14:59:26 +0200

    added a case for EDIT_EMPLOYEE and COMPLETED

--- employer-tracker/client/src/redux/reducer/formReducer.js
+++ employer-tracker/client/src/redux/reducer/formReducer.js
@@ -1,4 +1,10 @@
-import { GET_EMPLOYEE } from "../actionTypes/form";
+import {
+  GET_EMPLOYEE,
+  ADD_EMPLOYEE,
+  DELETE_EMPLOYEE,
+  Edit_EMPLOYEE,
+  COMPLETED,
+} from "../actionTypes/form";
 
 const initialState = {
   form: [],
@@ -10,16 +16,32 @@ export const formReducer = (state = initialState, action) => {
         ...state,
         form: action.payload,
       };
-    case "ADD_EMPLOYEE":
+    case ADD_EMPLOYEE:
       return {
         ...state,
         form: [...state.form, action.payload],
       };
-    case "DELETE_EMPLOYEE":
+    case DELETE_EMPLOYEE:
       return {
         ...state,
         form: state.form.filter((employee) => employee.id !== action.payload),
       };
+    case Edit_EMPLOYEE:
+      const { id, edit } = action.payload;
+      return {
+        ...state,
+        form: state.form.map((employee) => {
+          if (employee.id === id) {
+            return {
+              ...employee,
+              progress: edit,
+            };
+          }
+          return employee;
+        }),
+      };
+    case COMPLETED:
+      return state;
     default:
       return state;
   }
