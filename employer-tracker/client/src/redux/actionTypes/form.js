2988f49a13d1b4d84549d34d5981d5198ed73a27
skosana721 <wonganiskosana@gmail.com>
2021-04-28 14:55:02 +0200

    added actionTypes for COMPLETED and EDIT_EMPLOYEE

--- employer-tracker/client/src/redux/actionTypes/form.js
+++ employer-tracker/client/src/redux/actionTypes/form.js
@@ -1,3 +1,5 @@
 export const GET_EMPLOYEE = "GET_EMPLOYEE";
 export const ADD_EMPLOYEE = "ADD_EMPLOYEE";
 export const DELETE_EMPLOYEE = "DELETE_EMPLOYEE";
+export const Edit_EMPLOYEE = "Edit_EMPlOYEE";
+export const COMPLETED = "COMPLETED";
