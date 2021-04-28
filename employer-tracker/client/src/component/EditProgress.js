5dccb1e6ff31e3917ea0d259c3b8cf89d9482060
skosana721 <wonganiskosana@gmail.com>
2021-04-28 14:49:25 +0200

    removed the updateValue function

--- employer-tracker/client/src/component/EditProgress.js
+++ employer-tracker/client/src/component/EditProgress.js
@@ -5,28 +5,12 @@ import { editEmployee } from "../redux/actions/form";
 function EditProgress({ id, progress, updateValue }) {
   const [edit, setEdit] = useState(progress);
   const dispatch = useDispatch();
-  const employees = useSelector((state) => state.form.form);
-
-  //   const updateValue = (id) => {
-  //     let currentProgress;
-  //     let selectedEmployee = employees.find((employee) => employee.id === id);
-  //     console.log("selected", selectedEmployee);
-  //     if (selectedEmployee) {
-  //       currentProgress = selectedEmployee.progress;
-  //     }
-  //     currentProgress = edit;
-  //     return { currentProgress, id };
-  //   };
 
   const handleEdit = (e) => {
     e.preventDefault();
-    let found = employees.filter((employee) => employee.id === id);
-    let data = updateValue(id, edit);
-    if (found) {
-      dispatch(editEmployee(data));
-    }
+    dispatch(editEmployee({ id, edit }));
   };
-  console.log("value", updateValue(id, edit));
+
   return (
     <div>
       <form onSubmit={handleEdit}>
