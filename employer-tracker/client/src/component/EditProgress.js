e34320b8d03ccec72f5702262eabc9928bc2a7dc
skosana721 <wonganiskosana@gmail.com>
2021-04-28 15:53:31 +0200

    removed the useSelector import

--- employer-tracker/client/src/component/EditProgress.js
+++ employer-tracker/client/src/component/EditProgress.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { editEmployee } from "../redux/actions/form";
 
 function EditProgress({ id, progress }) {
